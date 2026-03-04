9918ac3e2da71d7a2bdb5eab3ae8f6ecbb5c3d66
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-03-05 02:03:08 +0800

    updated list and detail views and added templates

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Book
 
-# Create your views here.
+class GenreListView(ListView):
+    model = Book
+    template_name = 'bookclub/genre_list.html'
+    context_object_name = 'genres'
+
+
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'bookclub/book_detail.html'
+    context_object_name = 'book'
