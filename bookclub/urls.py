dd4f12330a9c5bd232b612ca7e104f522dfaafd5
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-03-05 02:09:28 +0800

    configured the urls for detail and list

---
+++ bookclub/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.BookListView.as_view(), name='book_list'),
+    path('book/<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+]
+
+app_name = 'bookclub'
