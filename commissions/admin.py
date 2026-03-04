0a292635a5b9e932ecac7fbf3686e998f5362086
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-04 11:52:27 +0800

    Register Commission and CommissionType in admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CommissionType, Commission
 
-# Register your models here.
+
+@admin.register(CommissionType)
+class CommissionTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Commission)
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = ("title", "people_required", "created_on", "updated_on")
+    search_fields = ("title", "description")
+    ordering = ("created_on",)
+    list_filter = ("created_on",)
