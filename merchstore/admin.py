3669c29ded2dcaa6b23355a8deb4bbf7b3fe20c7
Phisograph <jahanaraparedes@gmail.com>
2026-03-04 21:00:16 +0800

    Added admin.py and connected to models.py

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import ProductType, Product
 
-# Register your models here.
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ('name', 'product_type', 'price')
+    list_filter = ('product_type',)
+    search_fields = ('name',)
+
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)
