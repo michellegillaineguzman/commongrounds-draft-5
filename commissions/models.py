7386fe7454e0bba0deaa6291c0ebc2ffbe324610
MatthewManalus <75908740+MatthewManalus@users.noreply.github.com>
2026-03-04 11:51:49 +0800

    Add Commission and CommissionType models

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class CommissionType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ["name"]  
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.PositiveIntegerField()  
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_on"]  
+
+    def __str__(self) -> str:
+        return self.title
