7ec9bef966d632d0da81f6f124ac3d7f4d8481b4
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-03-05 01:51:55 +0800

    added app in settings.py, added genre and book models, added models to admin, and make migration

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Genre, Book
 
-# Register your models here.
+class GenreAdmin(admin.ModelAdmin):
+    model = Genre
+
+class BookAdmin(admin.ModelAdmin):
+    model = Book
+    list_display = ('title', 'author', 'publication_year', 'genre')
+    list_filter = ('genre', 'publication_year')
+    search_fields = ('title', 'author')
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Book, BookAdmin)
