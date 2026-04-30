e7ee584cdf90dfb6964a862b68cdc4aa4b5db9a1
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-04-30 22:23:35 +0800

    added templates for accounts, profile_update, login, and logout and edited url.py in accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .views import profile_update, register
 
 urlpatterns = [
-    path('register/', register, name='register'),
-    path('<str:username>/', profile_update, name='profile_update'),
+    path('register', register, name='register'),
+    path('<str:username>', profile_update, name='profile_update'),
 ]
 
 app_name = 'accounts'
