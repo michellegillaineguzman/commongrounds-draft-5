c5595cd9d3c834601aebb9d16aff50930c041a78
Elija Matthew Manalus <elija.manalus@student.ateneo.edu>
2026-05-03 04:21:03 +0800

    Add final commissions routes

--- commissions/urls.py
+++ commissions/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 app_name = "commissions"
@@ -6,4 +7,7 @@ app_name = "commissions"
 urlpatterns = [
     path("requests", views.CommissionListView.as_view(), name="request_list"),
     path("request/<int:pk>", views.CommissionDetailView.as_view(), name="request_detail"),
+    path("request/add", views.CommissionCreateView.as_view(), name="request_create"),
+    path("request/<int:pk>/edit", views.CommissionUpdateView.as_view(), name="request_update"),
+    path("job/<int:pk>/apply", views.ApplyToJobView.as_view(), name="job_apply"),
 ]
