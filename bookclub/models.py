1f8edd6e5e0127ac83a7a3f8d5796168c4b66444
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-05-03 09:38:17 +0800

    updated the models and migrations

--- bookclub/models.py
+++ bookclub/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import timedelta
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=100, default='Reader')
+
+    def __str__(self):
+        return self.name
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
@@ -14,8 +23,11 @@ class Genre(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=255)
     genre = models.ForeignKey(Genre, null=True, blank=True, on_delete=models.SET_NULL, related_name='books')
+    contributor = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
     author = models.CharField(max_length=255)
+    synopsis = models.TextField(null=True, blank=True)
     publication_year = models.IntegerField()
+    available_to_borrow = models.BooleanField(default=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -24,3 +36,22 @@ class Book(models.Model):
 
     class Meta:
         ordering = ['-publication_year']
+
+class BookReview(models.Model):
+    user_reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True)
+    anon_reviewer = models.CharField(null=True, blank=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    title = models.CharField(max_length=255)
+    comment = models.TextField()
+
+class Bookmark(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date_bookmarked = models.DateTimeField(auto_now_add=True)
+
+class Borrow(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrower = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, null=True)
+    date_borrowed = models.DateTimeField()
+    due_date = models.DateTimeField()
