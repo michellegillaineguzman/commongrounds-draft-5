1e17ab175b5062756100cd2605845bcf4d789b8d
angelakyrasalarda-prog <angela.kyra.salarda@student.ateneo.edu>
2026-05-06 13:58:50 +0800

    debugged model, views, forms, urls

--- bookclub/admin.py
+++ bookclub/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Genre, Book
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Genre, Book, Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline]
 
 class GenreAdmin(admin.ModelAdmin):
     model = Genre
@@ -10,5 +19,7 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('genre', 'publication_year')
     search_fields = ('title', 'author')
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Book, BookAdmin)
