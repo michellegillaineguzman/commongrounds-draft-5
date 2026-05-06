e724e22bcaa9302747f8ba1fb1662dca31af29d9
Sam Daradar <samantha.noryn.daradar@student.ateneo.edu>
2026-05-06 14:11:46 +0800

    apply styling to diyprojects app

--- diyprojects/forms.py
+++ diyprojects/forms.py
@@ -2,11 +2,30 @@ from django import forms
 from .models import Project, ProjectRating, ProjectReview
 
 class ProjectForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.Select):
+                field.widget.attrs.update({'class': 'form-select form-select-cg'})
+            else:
+                field.widget.attrs.update({'class': 'form-control form-control-cg'})
+
     class Meta:
         model = Project
         fields = ['title', 'category', 'description', 'materials', 'steps']
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'e.g. Handmade Ceramic Vase'}),
+            'description': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Briefly describe your project...'}),
+            'materials': forms.Textarea(attrs={'rows': 3, 'placeholder': 'List the materials needed, e.g. Clay, Pottery Wheel, Kiln...'}),
+            'steps': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Describe the steps to complete your project...'}),
+        }
 
 class ProjectRatingForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control form-control-cg'})
+
     class Meta:
         model = ProjectRating
         fields = ['score']
@@ -15,6 +34,11 @@ class ProjectRatingForm(forms.ModelForm):
         }
 
 class ProjectReviewForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control form-control-cg'})
+
     class Meta:
         model = ProjectReview
         fields = ['comment', 'image']
